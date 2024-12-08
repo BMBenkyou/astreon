406719cb0441e39e28a5215e030091902915b64c
BMBenkyou <178959326+BMBenkyou@users.noreply.github.com>
2024-12-09 07:23:04 +0800

    Add api endpoint for flashcards and quizzes

--- backend/chatbot/urls.py
+++ backend/chatbot/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import ChatWithGeminiView,get_user_schedule,UserSessionsView,QuizListView
+from .views import ChatWithGeminiView,get_user_schedule,UserSessionsView,QuizListView,FlashcardListView
 
 urlpatterns = [
     path('', ChatWithGeminiView.as_view(), name='chat_with_gemini'),
     path('schedule/<str:username>/',get_user_schedule, name="get_user_schedule"),
     path('session/', UserSessionsView.as_view(), name="user_sessions"),
-     path('quizzes/<int:id>', QuizListView.as_view(), name='quiz-list'), 
+    path('quizzes/<int:quiz_id>', QuizListView.as_view(), name='quiz-list'), 
+    path('flashcards/<int:flashcard_id>', FlashcardListView.as_view(), name='flashcard-list'), 
 
 ]
