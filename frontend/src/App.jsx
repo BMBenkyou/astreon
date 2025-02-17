0e946e460f1451f0fdc8066c9de5cf1e9be57f5b
BMBenkyou <178959326+BMBenkyou@users.noreply.github.com>
2025-02-17 18:33:21 +0800

    Modify backend volume

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1> mamaVite + React how about thisss updatinggg testtt againnn test</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
