e46ebcfdf92e54ac849d5a7f17b43992c305def7
Micah Sereno <micah.sereno@gmail.com>
2025-03-02 13:54:07 +0800

    unfinished

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,15 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { HomePage } from "./pages/Homepage.jsx";
+import { Aichat } from './pages/ai-chat.jsx';
+
 
 function App() {
   return (
       <Router>
           <Routes>
               <Route path="/" element={<HomePage />} />
-              <Route path="/chatbox" element={<HomePage />} />
+              <Route path="/HomePage" element={<HomePage />} />
+                <Route path="/Aichat" element={<Aichat />} />
 
               </Routes>
         </Router>
