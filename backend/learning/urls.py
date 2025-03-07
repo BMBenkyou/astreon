d640c85ebcf6112086d49a37d9e418795a14a80c
BMBenkyou <178959326+BMBenkyou@users.noreply.github.com>
2025-03-07 23:55:08 +0800

    Configure url mapping

--- backend/learning/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import create_quiz, get_quizzes, QuizGenerationView
-
-urlpatterns = [
-    path("quizzes/create/", create_quiz, name="create_quiz"),
-    path("quizzes/generate/", QuizGenerationView.as_view(), name="generate_quiz"),
-    path("quizzes/", get_quizzes, name="get_quizzes"),
-]
--- backend/user/urls.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from django.urls import path,include
-from . views import get_csrf_token
-urlpatterns = [
-    path("get-csrf-token/", get_csrf_token, name="get_csrf_token"),
-]
