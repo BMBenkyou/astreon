09e0a30a9dcabc32e8782cf91f045b7ba271e7f7
Micah Sereno <micah.sereno@gmail.com>
2025-03-07 23:38:20 +0800

    finish chatbox

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { HomePage } from "./pages/Homepage.jsx";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import HomePage  from "./pages/Homepage";
 import Login from "./pages/login"; 
 import Signup from "./pages/signup";
 import Passwordreset from "./pages/passwordreset";
@@ -8,32 +9,28 @@ import GenerateSchedule from "./pages/genschedule";
 import Profile from "./pages/profile";
 import Streak from "./pages/streak";
 import PSettings from "./pages/psettings";
-import Home from "./pages/home";
 import Quiz from "./pages/quiz";
-import { Aichat } from './pages/ai-chat.jsx';
-
-function App() {
-  return (
-      <Router>
-          <Routes>
-              <Route path="/" element={<HomePage />} />
-              <Route path="/homepage" element={<HomePage />} />
-              <Route path="/ai-chat" element={<Aichat />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/signup" element={<Signup />} />
-              <Route path="/passwordreset" element={<Passwordreset />} />
-              <Route path="/newpass" element={<Newpassword />} />
-              <Route path="/genschedule" element={<GenerateSchedule />} />
-              <Route path="/quiz" element={<Quiz />} />
-              <Route path="/profile" element={<Profile />} />
-              <Route path="/streak" element={<Streak />} />
-              <Route path="/psettings" element={<PSettings />} /> 
+import Aichat from "./pages/ai-chat";
 
 
-              </Routes>
-        </Router>
-    );
-}
+const App = () => (
+  <div className="app-container">
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/homepage" element={<HomePage />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/passwordreset" element={<Passwordreset />} />
+      <Route path="/newpass" element={<Newpassword />} />
+      <Route path="/genschedule" element={<GenerateSchedule />} />
+      <Route path="/quiz" element={<Quiz />} />
+      <Route path="/profile" element={<Profile />} />
+      <Route path="/streak" element={<Streak />} />
+      <Route path="/psettings" element={<PSettings />} />
+      <Route path="/ai-chat" element={<Aichat />} />
+    </Routes>
+  </div>
+);
 
 export default App;
 
